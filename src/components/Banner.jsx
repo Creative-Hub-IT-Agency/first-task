735980ff1a06b6048dea7669b398ff6ed534ecdb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-01 14:19:16 +0600

    navbar and notice fully responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     return (
         <div className="">
             <div className="">
-                <img src="https://i.ibb.co.com/TBbyn4pB/university-admission-web-banner-1281315-475.jpg" alt="" className="md:min-w-[700px] lg:w-[1152px] lg:h-[550px] mx-auto" />
+                <img src="https://i.ibb.co.com/TBbyn4pB/university-admission-web-banner-1281315-475.jpg" alt="" className="w-full h-[500px] lg:h-[550px] mx-auto" />
 
             </div>
             <marquee direction="left" className="bg-red-800"><button className="bg-[#172d5e] text-white font-semibold rounded-md px-4 py-2 ml-16 mr-4">News</button><span className="text-white ">*** Admissions Open! Apply Now to Secure Your Spot for the Upcoming Session *** Admissions Open! Apply Now to Secure Your Spot for the Upcoming Session *** Admissions Open! Apply Now to Secure Your Spot for the Upcoming Session ***</span></marquee>
