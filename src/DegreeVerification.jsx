d4c03641f299016c02ca8ec70c3b30662fab3fc7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-03 13:14:38 +0600

    degree verification page created with navbar and footer

---
+++ src/DegreeVerification.jsx
@@ -0,0 +1,14 @@
+import FilterOption from "./components/FilterOption"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+const DegreeVerification = () => {
+  return (
+    <div>
+      <Navbar />
+      <FilterOption/>
+      <Footer/>
+    </div>
+  )
+}
+export default DegreeVerification
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css'
 import App from './App.jsx'
-import FilterOption from './components/FilterOption.jsx';
+import DegreeVerification from './DegreeVerification.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='/degree-verification' element={<FilterOption />} />
+        <Route path='/degree-verification' element={<DegreeVerification/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
